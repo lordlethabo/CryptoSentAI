a6bee47b8e875efcd2e035b927989c59dea5bd62
Lethabo Mafihle James Moshabane <Getintouchwithlethabo@gmail.com>
2026-03-19 06:18:39 +0200

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,15 +1,10 @@
 %%writefile src/__init__.py
-
 """
-CryptoSentAI package initializer
+CryptoSentAI source package
 """
 
+# Expose key modules for easy import
 from .data_ingestion import fetch_crypto_prices
 from .feature_engineering import build_features
-from .ml_models import train_models, predict_next_price
-from .lstm_model import train_lstm, predict_lstm
 from .sentiment_model import analyze_sentiment
-from .confidence import calculate_confidence
-from .trading_strategy import generate_signal
-from .backtesting import backtest_strategy
-from .pipeline import run_pipeline
+from .ml_models import train_models, predict_next_price
